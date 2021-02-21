56936ca33c5c8ef1d2d78268a2724990a44d9cbe
Amanda M. Jessup <mandajessup@gmail.com>
2021-02-20 19:17:55 -0500

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the number of cards to choose from: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                
+
+                foreach (string card in CarkPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+                Console.ReadLine();
+                               
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number buddy!");
+            }
+        }
+    }
+}
